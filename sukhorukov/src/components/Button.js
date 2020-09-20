b5ddb1808d467dd7bcc977a397df5c399c53dd3c
rusla <suhrugen@gmail.com>
2020-09-20 21:32:07 +0700

    удаление ;

--- sukhorukov/src/components/Button.js
+++ sukhorukov/src/components/Button.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Button = ({children}) => {
     const handleClick = (event) => {
         console.log(event);    
-    };
+    }
 
     return (
         <button onClick={handleClick}>{children}</button>
-    );
-};
+    )
+}
 
 export default Button
